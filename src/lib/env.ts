978014a2eaee57e103fb695fe9250a77ecdb8999
min33sky <min33sky@naver.com>
2023-04-22 22:22:07 +0900

    feat: env type

---
+++ src/lib/env.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+const envVariables = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']),
+  DATABASE_URL: z.string(),
+  BASE_URL: z.string(),
+  GITHUB_ID: z.string(),
+  GITHUB_SECRET: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  NEXTAUTH_JWT_SECRET: z.string(),
+  NEXTAUTH_SECRET: z.string(),
+});
+
+type EnvVariables = z.infer<typeof envVariables>;
+//      ^?
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends EnvVariables {}
+  }
+}
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,12 +10,12 @@ export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     CredentialsProvider({
       name: 'credentials',
