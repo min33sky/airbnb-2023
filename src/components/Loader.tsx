fac8d44662e8d099628316b32e080b0369c1030a
min33sky <min33sky@naver.com>
2023-05-06 04:33:55 +0900

    feat: package update

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,15 +4,7 @@ import { PuffLoader } from 'react-spinners';
 
 const Loader = () => {
   return (
-    <div
-      className="
-      flex
-      h-[70vh]
-      flex-col
-      items-center
-      justify-center
-    "
-    >
+    <div className="flex h-[70vh] flex-col items-center justify-center">
       <PuffLoader size={100} color="red" />
     </div>
   );
